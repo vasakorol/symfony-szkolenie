cfeb514d5d5136d7ecf3357ebd986e2ec2d3e611
vasakorol <vasakorol@gmail.com>
2017-04-06 13:52:57 +0200

    LoadUserData

--- src/AppBundle/DataFixtures/ORM/LoadUserData.php
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -2,9 +2,27 @@
 
 namespace AppBundle\DataFixtures\ORM;
 
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
-
-class LoadUserData
+class LoadUserData extends AbstractFixture implements ContainerAwareInterface
 {
+    use ContainerAwareTrait;
 
+    public function load(ObjectManager $manager) {
+
+        $userManager = $this->container->get('fos_user.user_manager');
+
+        $user = $userManager->createUser();
+        $user->setUsername('user');
+        $user->setEmail('email@email.com');
+        $user->setPlainPassword('password');
+        $user->setEnabled(true);
+        $user->setRoles(['ROLE_USER']);
+        $user->setReference('user', $user);
+
+        $userManager->updateUser($user, true);
+    }
 }
