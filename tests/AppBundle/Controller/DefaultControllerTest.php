cd419150da063e60c0eb45d85b43dd8fbc0cc174
ksiatka <kamil.siatka@polcode.pl>
2017-04-06 15:06:48 +0200

    test controller

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use AppBundle\DataFixtures\ORM\LoadUserData;
+use AppBundle\Entity\User;
+use Liip\FunctionalTestBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+    /*
     public function testIndex()
     {
         $client = static::createClient();
@@ -14,5 +17,51 @@ class DefaultControllerTest extends WebTestCase
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+    }
+    */
+    
+    public function testIndexNotLogged()
+    {
+        $fixtures = $this->loadFixtures([LoadUserData::class])->getReferenceRepository();
+        
+        $client = $this->makeClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertStatusCode(302, $client);
+    }
+
+    public function testIndexLogged()
+    {
+        $fixtures = $this->loadFixtures([LoadUserData::class])->getReferenceRepository();
+        $user = $fixtures->getReference('user');
+        
+        $client = $this->makeClient(['username' => $user->getUsername(), 'password' => 'tester']);
+        $crawler = $client->request('GET', '/');
+
+        $this->assertStatusCode(200, $client);
+    }
+    
+    
+    /**
+     * $group functional
+     */
+    public function testIndex() {
+        $fixtures=$this->loadFixtures([
+            LoadUserData::class
+        ])->getReferanceRepository();
+        
+        $user = $fixtures->getReferance('user');
+        
+        /**
+         * @var $user User
+         */
+        $client = $this->makeClient([
+            'username'=>$user->getUsername(),
+            'password'=>'password',
+        ]);
+        
+        $crawler = $client->request('GET', '/');
+        $this->assertStatusCode(200, $client);
+        //$this->assertContains('No tasks', $crawler->filter());
     }
 }
